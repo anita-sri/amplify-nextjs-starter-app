a7c118184de9d4d66f3a1d4edfd5396f22c9ff01
Anita Srivastava <116413266+anita-sri@users.noreply.github.com>
2024-02-27 11:25:44 -0500

    Update page.tsx
    text field

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import {TextAreaField} from '@aws-amplify/ui-react'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.center}>
-        <Flex>
+        <TextAreaField>
           <Label>Provide Business Summary: </Label>
-          <input id="business_summary" isRequired={true}></input>
-        </Flex>
+          <id="business_summary" isRequired={true}></id>
+        </TextAreaField>
       </div>
     </main>
   )
