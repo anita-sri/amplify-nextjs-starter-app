f566b9c7ed8dd995350234e0e74eb9ec87bacd3f
Anita Srivastava <116413266+anita-sri@users.noreply.github.com>
2024-02-27 11:41:33 -0500

    Update page.tsx
    import amplify

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import {TextAreaField} from '@aws-amplify/ui-react'
+import Image from 'next/image';
+import styles from './page.module.css';
+import Amplify {TextAreaField} from '@aws-amplify/ui-react';
 
 export default function Home() {
   return (
