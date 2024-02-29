08441960a232de42430a68e99e82a77df568e751
anita-sri <anitasri@amazon.com>
2024-02-29 15:07:35 +0000

    removed button

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import '@aws-amplify/ui-react';
+import '@aws-amplify';
 
 function App() {
     return (
         <div>  
             <h2> Provide Buiness Summary: </h2>
             <textarea></textarea>
-            <button>Submit</button>
         </div>
         )
 }
